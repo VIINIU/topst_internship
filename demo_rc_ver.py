efab023e39b02d7118529cd306284cb70e58e486
박소연 (SoYeon Park) <soyeon.park@telechips.com>
2026-01-23 11:48:45 +0900

    flag 추가

--- demo_rc_ver.py
+++ demo_rc_ver.py
@@ -160,7 +160,8 @@ _can = {"park": False,
         "is_braking": False,
         "is_resverse": False,
         "is_steering": False,
-        "is_steer_reverse": False}  
+        "is_steer_reverse": False
+        "avoid_mode": False,} #[소연] 장애물 회피 플래그  
 
 _lock   = threading.Lock()
 _stop   = False
